925eab26b5648b264f82d720f7abab57c4f62d53
Patrick Taylor <patchfalcon+github@gmail.com>
2015-11-26 22:07:17 +0000

    Remove console.log

--- js/bluesky.js
+++ js/bluesky.js
@@ -5,6 +5,5 @@ $(document).ready(function() {
 $(window).load(function() {
   // Set active navigation item
   var url = window.location.pathname;
-  console.log(url);
   $('.header ul.nav a[href="'+ url +'"]').addClass('active');
 });
