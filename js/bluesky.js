8cd0943d98324e7f7d6546febde95d8292c8258d
Patrick Taylor <patchfalcon+github@gmail.com>
2015-11-30 22:28:26 +0000

    Display active item on mobile navigation

--- js/bluesky.js
+++ js/bluesky.js
@@ -5,5 +5,5 @@ $(document).ready(function() {
 $(window).load(function() {
   // Set active navigation item
   var url = window.location.pathname;
-  $('.header ul.nav a[href="'+ url +'"]').addClass('active');
+  $('.header ul.nav a[href="'+ url +'"]').parent().addClass('active');
 });
